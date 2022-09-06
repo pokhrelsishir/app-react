be708468f611c48159da5ec0dbf743228be0a604
Sishir Pokhrel <pokhrelsishir@gmail.com>
2022-09-05 22:15:03 -0500

    Table Components Organization

--- src/Table.js
+++ src/Table.js
@@ -1,37 +1,46 @@
-import React, { Component } from 'react'
-
-class Table extends Component {
-  render() {
+const TableHeader = () => {
     return (
-
-      <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Hobby</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Jack</td>
-            <td>Drawing</td>
-          </tr>
-          <tr>
-            <td>John</td>
-            <td>Travelling</td>
-          </tr>
-          <tr>
-            <td>Monika</td>
-            <td>Acting</td>
-          </tr>
-          <tr>
-            <td>James</td>
-            <td>Singing</td>
-          </tr>
-        </tbody>
-      </table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Hobby</th>
+        </tr>
+      </thead>
     )
   }
-}
 
-export default Table
+
+  const TableBody = () => {
+    return (
+      <tbody>
+        <tr>
+          <td>Jack</td>
+          <td>Drawing</td>
+        </tr>
+        <tr>
+          <td>John</td>
+          <td>Travelling</td>
+        </tr>
+        <tr>
+          <td>Monica</td>
+          <td>Acting</td>
+        </tr>
+        <tr>
+          <td>James</td>
+          <td>Singing</td>
+        </tr>
+      </tbody>
+    )
+  } 
+
+
+  class Table extends Component {
+    render() {
+      return (
+        <table>
+          <TableHeader />
+          <TableBody />
+        </table>
+      )
+    }
+  }
