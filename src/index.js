ac71846631d1cc9bbdfd9e0a8eb370448e09024c
Sishir Pokhrel <pokhrelsishir@gmail.com>
2022-09-05 21:59:10 -0500

    Seperated App Component into Class

--- src/index.js
+++ src/index.js
@@ -1,15 +1,7 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
+import App from './App'
 import './index.css'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Hello World from React !!!</h1>
-      </div>
-    )
-  }
-}
-//Comment
+
 ReactDOM.render(<App />, document.getElementById('root'))
